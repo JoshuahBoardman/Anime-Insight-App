e3c97de63c15a8ce92b6084d3ba0365702ece7b4
JoshuahBoardman <joshuahboardman@gmail.com>
2022-03-29 16:45:25 -0500

    The application now pulls data from the Kitsu API.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 
 function App() {
+  const BASE_API_PATH = "https://kitsu.io/api/edge";
+
+  const [trendingAnimeData, setTrendingAnimeData] = useState([])
+
+  useEffect(() =>  {
+    handleTrendingAnimeData();
+    console.log(trendingAnimeData)
+  }, [])
+
+  async function getTrendingAnimeData() {
+    try {
+      const response = await fetch(`${BASE_API_PATH}/trending/anime`);
+      if(response.status !== 200) return
+      const data = await response.json();
+      // console.log(data);
+      return data
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function handleTrendingAnimeData() {
+    const data = await getTrendingAnimeData();
+    setTrendingAnimeData([...data.data]);
+  }
+
+
   return (
     <>
       <Header />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ const Header = () => {
         <nav className="navbar navbar-dark bg-primary">
             <div className="container fluid">
                 <div className="navbar-brand">Anime Insight</div>
-                <form class="d-flex">
-                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <form className="d-flex">
+                  <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                   {/* TODO: change search for a eye-glass, maybe a input-group*/}
-                  <button class="btn btn-outline-light" type="submit">Search</button>
+                  <button className="btn btn-outline-light" type="submit">Search</button>
                 </form>
             </div>
         </nav>
