ee13b8c711e5d3f4422643da1b83bc9d21598c96
JoshuahBoardman <joshuahboardman@gmail.com>
2022-04-04 14:17:59 -0500

    Added routing to the Header component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
 
   return (
     <header>
       <nav className="navbar navbar-dark bg-primary shadow">
         <div className="container fluid">
           {/* TODO: maybe add a book with a eye as a logo */}
-          <div className="navbar-brand">Anime Insight</div>
+          <Link to="/">
+            <div className="navbar-brand">Anime Insight</div>
+          </Link>
           <form className="d-flex">
             <input
               className="form-control me-2"
@@ -16,7 +19,7 @@ const Header = () => {
               placeholder="Search"
               aria-label="Search"
               value={inputValue}
-              onChange={e => setInputValue(e.target.value)}
+              onChange={(e) => setInputValue(e.target.value)}
             />
             {/* TODO: change search for a eye-glass, maybe a input-group*/}
             <button className="btn btn-outline-light" type="submit">
